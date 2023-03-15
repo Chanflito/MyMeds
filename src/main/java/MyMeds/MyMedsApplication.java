c069794c0e1fd61d7194dd90616dc795262290b1
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2023-03-15 12:19:21 -0300

    Initial commit

---
+++ src/main/java/MyMeds/MyMedsApplication.java
@@ -0,0 +1,13 @@
+package MyMeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMedsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMedsApplication.class, args);
+	}
+
+}
